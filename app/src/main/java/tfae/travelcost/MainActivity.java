2ab40d6f5afb93d9fc6da049640da4a0fb23980c
Tiago Eusébio <tiago.eusebio@peakit.pt>
2017-12-29 03:24:38 +0000

    Add new field: multiply the cost by…

--- app/src/main/java/tfae/travelcost/MainActivity.java
+++ app/src/main/java/tfae/travelcost/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         final EditText edtConsumption = (EditText) findViewById(R.id.edtConsumption);
         final EditText edtPeopleNr    = (EditText) findViewById(R.id.edtPeopleNr);
         final EditText edtTolls       = (EditText) findViewById(R.id.edtTolls);
+        final EditText edtTimes       = (EditText) findViewById(R.id.edtTimes);
         final Button btnClear         = (Button) findViewById(R.id.btnClear);
         final Button btnCalculate     = (Button) findViewById(R.id.btnCalculate);
         final TextView txtResultLabel = (TextView) findViewById(R.id.txtResultLabel);
@@ -32,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
                 edtConsumption.setText("");
                 edtPeopleNr.setText("");
                 edtTolls.setText("");
+                edtTimes.setText("");
 
                 txtResultLabel.setText("");
                 txtResultValue.setText("");
@@ -45,24 +47,26 @@ public class MainActivity extends AppCompatActivity {
                 Float consumption = getFloatValue(edtConsumption, 0);
                 Integer people    = getIntegerValue(edtPeopleNr, 1);
                 Float tolls       = getFloatValue(edtTolls, 0);
+                Integer times     = getIntegerValue(edtTimes, 1);
 
                 edtDistance.setText(distance.toString());
                 edtPrice.setText(price.toString());
                 edtConsumption.setText(consumption.toString());
                 edtPeopleNr.setText(people.toString());
                 edtTolls.setText(tolls.toString());
+                edtTimes.setText(times.toString());
 
                 txtResultLabel.setText(getResources().getString(R.string.total_per_person));
-                txtResultValue.setText(calculate(distance, price, consumption, people, tolls));
+                txtResultValue.setText(calculate(distance, price, consumption, people, tolls, times));
             }
         });
 
     }
 
-    private String calculate(Float distance, Float price, Float consumption, Integer people, Float tolls) {
+    private String calculate(Float distance, Float price, Float consumption, Integer people, Float tolls, Integer times) {
         Float gas  = (distance * consumption)/100;
         Float cost = (gas * price) + tolls;
-        return round(cost / people) + getResources().getString(R.string.cur_symbol);
+        return round((cost * times) / people) + getResources().getString(R.string.cur_symbol);
     }
 
     private Float getFloatValue(EditText edt, Integer default_value) {
