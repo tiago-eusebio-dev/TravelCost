535d2b7ed33e77e2ad05646a9e4c6fe8fdb5ee0a
Tiago Eusébio <tfaeusebio@gmail.com>
2018-09-22 23:53:02 +0100

    Update TargetAPI to 28 and add a ScrollView to the main Activity

--- app/src/main/java/tfae/travelcost/MainActivity.java
+++ app/src/main/java/tfae/travelcost/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
